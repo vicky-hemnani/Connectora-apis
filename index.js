58061f06fa8f09cb9ac61fc7301c1b4a8e04f0b8
vicky-hemnani <58837151+vicky-hemnani@users.noreply.github.com>
2021-06-17 14:11:38 +0530

    commited  index file

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 
+const conversationRoute=require("./routes/conversation");
+const messageRoute=require("./routes/message");
+const multer=require("multer");
+const path=require("path")
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }).then(() => {
@@ -16,13 +21,38 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useCreateIndex:
     console.log("No connection");
 });
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+
 //middlewares
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+/routes
 app.use("/api/users", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
+app.use("/api/conversations",conversationRoute);
+app.use("/api/messages",messageRoute);
 
 app.get("/", (req, res) => {
     res.send("Welcome to home page");
